dec71ce1bd01fc989c429e884a8e329e94305854
raymondtju <raymond.rtju@gmail.com>
2023-10-05 20:52:00 +0700

    feat: quiz logic

--- app/(app)/dashboard/(kelas)/class/[slug]/(detail-course)/detail-course/quiz/[soal]/page.tsx
+++ app/(app)/dashboard/(kelas)/class/[slug]/(detail-course)/detail-course/quiz/[soal]/page.tsx
@@ -1,73 +1,89 @@
-"use client"
+"use client";
 
-import { cn } from "@/lib/utils"
-import { useState } from "react"
+import { cn } from "@/lib/utils";
+import { useState } from "react";
 
 const answerButton = [
-   {
-      title: "Meningkatkan performa server",
-      value: 0,
-   },
-   {
-      title: "Memaksimalkan keuntungan bisnis",
-      value: 0
-   },
-   {
-      title: "Memahami dan meningkatkan pengalaman pengguna",
-      value: 1
-   },
-   {
-      title: "Meningkatkan keamanan data",
-      value: 0
-   }
-]
+  {
+    label: "Meningkatkan performa server",
+    isAnswer: false,
+  },
+  {
+    label: "Memaksimalkan keuntungan bisnis",
+    isAnswer: false,
+  },
+  {
+    label: "Memahami dan meningkatkan pengalaman pengguna",
+    isAnswer: true,
+  },
+  {
+    label: "Meningkatkan keamanan data",
+    isAnswer: false,
+  },
+];
 
 function Quiz({ params }: { params: { soal: string } }) {
-   console.log(params.soal)
+  // console.log(params.soal);
 
+  const trueAns = answerButton.findIndex((item) => item.isAnswer);
+  const [dataForm, setDataForm] = useState<{
+    selected: number | null;
+    isAnswerTrue: boolean;
+  }>({
+    selected: null,
+    isAnswerTrue: false,
+  });
+  console.log(dataForm);
 
-   const [select, isSelect] = useState(0)
-   const [ans] = useState(1)
-
-
-   return (
-      <>
-         <div className="grid gap-6">
-            <h5 className="text-center font-semibold text-2xl">
-               Apa tujuan utama dari UX Research
-               <span className="sr-only">{params.soal}</span>
-            </h5>
-            <div className="flex justify-center gap-4">
-               <div className="max-w-2xl w-[30rem]">
-                  <div className="rounded-3xl h-[19rem] bg-gray-200"></div>
-               </div>
-               <div className="w-[40rem] flex flex-col gap-4 px-4">
-                  {answerButton.map((item, index) => (
-                     <button
-                        onClick={() => {
-                           isSelect(index)
-                        }}
-                        value={item.value}
-                        key={index}
-                        type="button"
-                        className={cn(
-                           "text-base flex items-center justify-between p-4 rounded-2xl border-[2px] border-[#E4E7EC] shadow-[#E4E7EC] shadow-[0px_4px_0px_0px] bg-[#FFF]",
-                           select === index ?
-                              "border-[#0F973D] shadow-[#0F973D]" :
-                              "border-[#DB2323] shadow-[#DB2323]"
-                        )}>
-                        {item.title}
-                        <span className=" w-7 h-7 rounded-md p-1 border grid place-content-center">
-                           {index + 1}
-                        </span>
-                     </button>
-                  ))}
-
-               </div>
-            </div>
-         </div>
-      </>
-   )
+  return (
+    <>
+      <div className="grid gap-6">
+        <h5 className="text-center text-2xl font-semibold">
+          Apa tujuan utama dari UX Research
+          <span className="sr-only">{params.soal}</span>
+        </h5>
+        <div className="flex justify-center gap-4">
+          <div className="w-[30rem] max-w-2xl">
+            <div className="h-[19rem] rounded-3xl bg-gray-200"></div>
+          </div>
+          <div className="flex w-[40rem] flex-col gap-4 px-4">
+            {answerButton.map((item, index) => (
+              <button
+                onClick={() => {
+                  setDataForm({
+                    ...dataForm,
+                    selected: index,
+                  });
+                  console.log(index);
+                  console.log(dataForm);
+                }}
+                key={index}
+                type="button"
+                disabled={dataForm.selected !== null}
+                className={cn(
+                  "flex items-center justify-between rounded-2xl border-[2px] border-[#E4E7EC] bg-[#FFF] p-4 text-base shadow-[0px_4px_0px_0px] shadow-[#E4E7EC]",
+                  dataForm.selected !== null &&
+                    index === dataForm.selected &&
+                    (answerButton[index].isAnswer
+                      ? "border-[#0F973D] shadow-[#0F973D]"
+                      : "border-[#DB2323] shadow-[#DB2323]"),
+                  dataForm.selected !== null &&
+                    !dataForm.isAnswerTrue &&
+                    index === trueAns &&
+                    "border-[#0F973D] shadow-[#0F973D]",
+                )}
+              >
+                {item.label}
+                <span className=" grid h-7 w-7 place-content-center rounded-md border p-1">
+                  {index + 1}
+                </span>
+              </button>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Quiz;
